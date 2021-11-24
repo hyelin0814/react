71a51974576cb05c06b57fbfe9bd61a94aefbcea
DESKTOP-D95L417\tj-bu <hyelin0814@gmail.com>
2021-11-24 13:49:59 +0900

    no message

---
+++ 01-1/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  let [name, setName] = useState('기메린') //let name = '기메린'
+  let fn = (e) => {
+    setName(e.target.innerText) //name = e.target.innerText
+  }
+  return (
+    <>
+      <p>안녕하세요 {name}입니다</p>
+
+      <button onClick={fn}>기메린구</button>
+      <button onClick={fn}>기메랑구</button>
+    </>
+  );
+};
+
+export default App;
--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+
 
 const App = () => {
-  let [name, setName] = useState('기메린') //let name = '기메린'
-  let fn = (e) => {
-    setName(e.target.innerText) //name = e.target.innerText
+  const[_num,_setNum]= useState(1)
+
+  const fn = () => {
+    _setNum(_num + 1)
   }
+
   return (
     <>
-      <p>안녕하세요 {name}입니다</p>
-
-      <button onClick={fn}>기메린구</button>
-      <button onClick={fn}>기메랑구</button>
+      <div>
+        <p> {_num}</p>
+        <button onClick={fn}>1씩 증가</button>
+      </div>
     </>
   );
 };
