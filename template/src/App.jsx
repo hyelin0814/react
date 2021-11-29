3a72bba7dcad0bc901f5ec3a9b6e6ed10b695f10
DESKTOP-D95L417\tj-bu <hyelin0814@gmail.com>
2021-11-29 10:36:47 +0900

    no message

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,19 +1,35 @@
 import React, { useRef, useState } from 'react';
 
-
 const App = () => {
-  const[_num,_setNum]= useState(1)
+  const [_manArr,_setManArr] = useState([])
+  const [_womenArr,_setWomenArr] = useState()
 
-  const fn = () => {
-    _setNum(_num + 1)
+  const refNameInput = useRef()
+  const refAgeInput = useRef()
+
+  const fnAddArr = ()=>{
+    let name = refNameInput.current.value
+    let age = refAgeInput.current.value
   }
+  const fnChangeGender = (e)=>{
+    let gender = e.target.value
 
+  }
   return (
     <>
-      <div>
-        <p> {_num}</p>
-        <button onClick={fn}>1씩 증가</button>
-      </div>
+      <p>이름,나이,성별을 입력하고 추가버튼을 누르면 성별이 구분되서 출력합니다</p>
+      이름 <input ref ={refNameInput}type="text" /> <br/>
+      나이 <input ref ={refAgeInput} type="text" /> <br/>
+      <input onChange={fnChangeGender} defaultValue="man" type="radio" name="gender"/>남  
+      <input onChange={fnChangeGender} defaultValue="woman" type="radio" name="gender"/>여
+      <br/>
+      <button onClick={fnAddArr}>추가하기</button>
+      <hr />
+      남자명단현황
+      <p></p>
+      <hr />
+      여자명단현황
+      <p></p>
     </>
   );
 };
