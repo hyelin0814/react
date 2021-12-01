8ec4b8143d7a409d27350462df31b207da61b5a2
DESKTOP-D95L417\tj-bu <hyelin0814@gmail.com>
2021-12-01 13:24:40 +0900

    no message

---
+++ 02-6배열/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [_arr, _setArr] = useState([])
+  const fnAdd = () => {
+    let key = Date.now()
+    _setArr([..._arr,key])
+  }
+  const fnDel = (e)=>{
+    let key = parseInt(e.target.getAttribute('deta-key'))
+    let arr = _arr.filter((v)=>{
+      return v!== key
+    })  
+    _setArr([...arr])
+  }
+  return (
+    <>
+      <button onClick={fnAdd}>배열추가</button>
+      <hr />
+
+      {
+        _arr.map((v)=>{
+          return(
+          <p key={v}>
+            {v}
+            <button  onClick={fnDel} deta-key={v}>삭제</button>
+          </p>
+          )
+        })
+      }
+    </>
+  );
+};
+
+export default App;
